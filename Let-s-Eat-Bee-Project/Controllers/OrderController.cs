88f348ff709437535b6e68c71b2f4f024f287ecd
darkmaiden <chursinatatianka@gmail.com>
2016-11-06 21:32:16 +0200

    fixing signin/signup forms

--- Let-s-Eat-Bee-Project/Controllers/OrderController.cs
+++ Let-s-Eat-Bee-Project/Controllers/OrderController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using static System.Net.WebRequestMethods;
+//using static System.Net.WebRequestMethods;
 using System.Data.Entity;
 using Let_s_Eat_Bee_Project.Models;
 
