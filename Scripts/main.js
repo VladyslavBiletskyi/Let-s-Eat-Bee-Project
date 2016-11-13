6dfe993f91e77d0fa8c433df46e806b678195aeb
darkmaiden <chursinatatianka@gmail.com>
2016-11-13 21:54:19 +0200

    kostyl for signin/signup validation

--- Scripts/main.js
+++ Scripts/main.js
@@ -17,3 +17,39 @@ $(document).ready(function () {
         vibrate: true // vibrate the device when dragging clock hand
     });
 });
+
+///for validation
+
+function validateEmail(email) {
+    var pattern = new RegExp("^[a-zA-Z]+([a-z]|\.|_|[0-9])+@[a-z]+\.[a-z]+$");
+    var res = pattern.test(email);
+    //alert(res);
+    if (!res) {
+        Materialize.toast('Invalid email format.', 3000);
+        return false;
+    }
+    return true;
+}
+function validatePassword(password) {
+    var pattern = new RegExp("^([a-zA-Z]|[0-9]){8,25}$");
+    var res = pattern.test(password);
+    //alert(res);
+    if (!res) {
+        Materialize.toast('Invalid password format. Use letters & digits (8-25).', 3000);
+        return false;
+    }
+    return true;
+}
+
+function validateName(name) {
+    var pattern = new RegExp("^([a-zA-Z]){4,20}$");
+    var res = pattern.test(name);
+    //alert(res);
+    if (!res) {
+        Materialize.toast('Invalid name format. Use letters only (4-20)', 3000);
+        return false;
+    }
+    return true;
+}
+
+// остальное пока удалено. не работает
