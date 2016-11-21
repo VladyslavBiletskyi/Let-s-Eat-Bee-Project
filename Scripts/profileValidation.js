a1684a3102b022acdec5b5a873fd1d08956a9027
darkmaiden <chursinatatianka@gmail.com>
2016-11-21 15:56:10 +0200

    fixed labels and validation

---
+++ Scripts/profileValidation.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $("#apply").attr("disabled", "true")
+
+    $("#first_name").blur(function () {
+        var first_name = $("#first_name").val();
+        var last_name = $("#last_name").val();
+
+        var valFirstName = validateName(first_name, true);
+        if (!valFirstName) {
+            $("#first_name").removeClass('valid');
+            $("#first_name").addClass('invalid');
+
+            $("#apply").attr("disabled", "true");
+            return;
+        }
+        $("#first_name").removeClass('invalid');
+        $("#first_name").addClass("valid");
+        if (valFirstName && validateName(last_name)) {
+            $("#apply").removeAttr("disabled");
+        }
+    });
+
+    $("#last_name").blur(function () {
+        var first_name = $("#first_name").val();
+        var last_name = $("#last_name").val();
+
+        var valLastName = validateName(last_name, true);
+        if (!valLastName) {
+            $("#last_name").removeClass('valid');
+            $("#last_name").addClass('invalid');
+
+            $("#apply").attr("disabled", "true");
+            return;
+        }
+        $("#last_name").removeClass('invalid');
+        $("#last_name").addClass("valid");
+        if (valLastName && validateName(first_name)) {
+            $("#apply").removeAttr("disabled");
+        }
+    });
+
+});
