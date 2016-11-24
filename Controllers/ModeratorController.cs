04a750cc2a421e42cba64e909e01b5dfb61dc566
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2016-11-24 15:44:02 +0200

    Moderator page is alive

--- Controllers/ModeratorController.cs
+++ Controllers/ModeratorController.cs
@@ -19,7 +19,68 @@ namespace Let_s_Eat_Bee_Project.Controllers
         // GET: Moderator
         public ActionResult Index()
         {
-            return View(new Let_s_Eat_Bee_Project.Models.DBDump { Orders=db.Orders.ToList(),Users=db.AllUsers.ToList()});
+            return View(new Let_s_Eat_Bee_Project.Models.DBDump {
+                Orders =db.Orders.ToList(),
+                AuthUsers =db.AllUsers.OfType<AuthorizedUser>().ToList(),
+                UnUsers =db.AllUsers.OfType<UnauthorizedUser>().ToList()});
+        }
+
+        [HttpPost]
+        public ActionResult UserDetail(int id)
+        {
+            AuthorizedUser user = db.AllUsers.OfType<AuthorizedUser>().FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView(user);
+        }
+        [HttpPost]
+        public ActionResult UserEdit(Let_s_Eat_Bee_Project.Models.AuthorizedUser user)
+        {
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                AuthorizedUser old = db.AllUsers.OfType<AuthorizedUser>().Where(x => x.Id == user.Id).FirstOrDefault();
+                old.FirstName = user.FirstName;
+                old.LastName = user.LastName;
+                old.Organization = user.Organization;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult OrderDetail(int id)
+        {
+            Order order = db.Orders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView(order);
+        }
+        [HttpPost]
+        public ActionResult OrderEdit(Let_s_Eat_Bee_Project.Models.Order order)
+        {
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Order old = db.Orders.Where(x => x.Id == order.Id).FirstOrDefault();
+                old.CreationDateTime = order.CreationDateTime;
+                old.Address = order.Address;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
         }
     }
 }
--- Models/ModeratorModels.cs
+++ Models/ModeratorModels.cs
@@ -6,7 +6,8 @@ namespace Let_s_Eat_Bee_Project.Models
     public class DBDump
     {
         public List<Order> Orders { get; set; }
-        public List<AbstractUser> Users { get; set; }
+        public List<AuthorizedUser> AuthUsers { get; set; }
+        public List<UnauthorizedUser> UnUsers { get; set; }
 
     }
 }
