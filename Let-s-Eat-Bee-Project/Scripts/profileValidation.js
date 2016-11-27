eca04dc47ae650adcc4e2700771403d5adaa30f5
darkmaiden <chursinatatianka@gmail.com>
2016-11-27 23:13:16 +0200

    fixed error page and changing password

--- Let-s-Eat-Bee-Project/Scripts/profileValidation.js
+++ Let-s-Eat-Bee-Project/Scripts/profileValidation.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
 
     var $form = $("#edit-profile-form");
+    var $form2 = $("#change-pass");
 
     $form.submit(function (e) {
         e.preventDefault();
@@ -32,4 +33,34 @@
         $form.get(0).submit();
     });
 
+    $form2.submit(function (e) {
+        e.preventDefault();
+
+        var pass = $("#new_password").val();
+        var conf = $("#conf_password").val();
+
+        var valPassw = validatePassword(pass, true);
+
+        if(valPassw)
+        {
+            markAsValid("#new_password");
+        }
+        if (pass === conf) {
+            markAsValid("#conf_password");
+        }
+
+        if (!valPassw || (pass !== conf))
+        {
+            if (!valPassw) {
+                markAsInvalid("#new_password");
+                markAsInvalid("#conf_password");
+            }
+            if (pass !== conf) {
+                markAsInvalid("#conf_password");
+            }
+            return;
+        }
+
+        $form2.get(0).submit();
+    });
 });
