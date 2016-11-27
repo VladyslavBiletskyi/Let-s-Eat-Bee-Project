d609d01bc32dbaec130840829b1100343d6091d0
darkmaiden <chursinatatianka@gmail.com>
2016-11-27 22:33:07 +0200

    fixed profile, deleted some files

--- Let-s-Eat-Bee-Project/Scripts/profileValidation.js
+++ Let-s-Eat-Bee-Project/Scripts/profileValidation.js
@@ -1,42 +1,35 @@
 ﻿$(document).ready(function () {
-    $("#apply").attr("disabled", "true")
 
-    $("#first_name").blur(function () {
+    var $form = $("#edit-profile-form");
+
+    $form.submit(function (e) {
+        e.preventDefault();
+
         var first_name = $("#first_name").val();
         var last_name = $("#last_name").val();
 
         var valFirstName = validateName(first_name, true);
-        if (!valFirstName) {
-            $("#first_name").removeClass('valid');
-            $("#first_name").addClass('invalid');
-
-            $("#apply").attr("disabled", "true");
-            return;
-        }
-        $("#first_name").removeClass('invalid');
-        $("#first_name").addClass("valid");
-        if (valFirstName && validateName(last_name)) {
-            $("#apply").removeAttr("disabled");
-        }
-    });
-
-    $("#last_name").blur(function () {
-        var first_name = $("#first_name").val();
-        var last_name = $("#last_name").val();
-
         var valLastName = validateName(last_name, true);
-        if (!valLastName) {
-            $("#last_name").removeClass('valid');
-            $("#last_name").addClass('invalid');
 
-            $("#apply").attr("disabled", "true");
+        if (valFirstName) {
+            markAsValid("#first_name");
+        }
+        if (valLastName) {
+            markAsValid("#last_name");
+        }
+
+        if (!valFirstName || !valLastName) {
+            Materialize.toast(toastContent, 3000);
+
+            if (!valFirstName) {
+                markAsInvalid("#first_name");
+            }
+            if (!valLastName) {
+                markAsInvalid("#last_name");
+            }
             return;
         }
-        $("#last_name").removeClass('invalid');
-        $("#last_name").addClass("valid");
-        if (valLastName && validateName(first_name)) {
-            $("#apply").removeAttr("disabled");
-        }
+        $form.get(0).submit();
     });
 
 });
--- Let-s-Eat-Bee-Project/Scripts/signinValidation.js
+++ Let-s-Eat-Bee-Project/Scripts/signinValidation.js
@@ -1,43 +1,7 @@
 ﻿$(document).ready(function () {
     //$("#signin").attr("disabled", "true");
     var $form = $("#signin-form");
-    /*$("#email").blur(function () {
-        var email = $("#email").val();
-        var password = $("#password").val();
-        var valEmail = validateEmail(email, true);
-        if(!valEmail)
-        {
-            $("#email").removeClass('valid');
-            $("#email").addClass('invalid');
 
-            $("#signin").attr("disabled", "true");
-            return;
-        }
-        $("#email").removeClass('invalid');
-        $("#email").addClass("valid");
-        if(valEmail && validatePassword(password))
-        {
-            $("#signin").removeAttr("disabled");
-        }
-    });
-
-    $("#password").blur(function () {
-        var email = $("#email").val();
-        var password = $("#password").val();
-        var valPassword = validatePassword(password, true);
-        if (!valPassword) {
-            $("#password").removeClass('valid');
-            $("#password").addClass('invalid');
-
-            $("#signin").attr("disabled", "true");
-            return;
-        }
-        $("#password").removeClass('invalid');
-        $("#password").addClass('valid');
-        if (valPassword && validateEmail(email)) {
-            $("#signin").removeAttr("disabled");
-        }
-    });*/
     $form.submit(function (e) {
         //alert($form.get(0));
         e.preventDefault();
